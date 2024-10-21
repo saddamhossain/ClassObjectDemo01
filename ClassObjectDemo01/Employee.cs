c86535d647f0770b7fb3b7022a5552e55db5d9eb
saddamhossain <saddamhossain953@gmail.com>
2024-10-22 00:32:24 +0600

    Add project files.

---
+++ ClassObjectDemo01/Employee.cs
@@ -0,0 +1,24 @@
+﻿namespace ClassObjectDemo01;
+
+public class Employee
+{
+    private const double DailyWorkingHours = 8.0;
+
+    private const double BonusMultiplier = 1.8;
+
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public double Salary { get; set; }
+    public int Experience { get; set; }
+
+    public double Work()
+    {
+        return DailyWorkingHours;
+    }
+
+    public double GetSalary()
+    {
+        return Salary * BonusMultiplier;
+    }
+}
---
+++ ClassObjectDemo01/Program.cs
@@ -0,0 +1,11 @@
+﻿Employee employee1 = new Employee();
+
+employee1.Id = "EMP-001";
+employee1.Name = "Saddam Hossain";
+employee1.Email = "saddamhossaindotnet@gmail.com";
+employee1.Salary = 50000;
+employee1.Experience = 10;
+
+Console.WriteLine($"Employee {employee1.Name} works {employee1.Work()} hours daily.");
+
+Console.WriteLine($"Employee {employee1.Name}'s adjusted salary: {employee1.GetSalary()}");
